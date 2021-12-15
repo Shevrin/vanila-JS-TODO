ff6278edb53186ecae4b2001deebb37ed688243f
pavel shevrin <77666606+Shevrin@users.noreply.github.com>
2021-12-16 00:52:02 +0300

    create app Todo

---
+++ script.js
@@ -0,0 +1,56 @@
+
+const todoControl = document.querySelector('.todo-control')
+const headerInput = document.querySelector('.header-input')
+const todoList = document.querySelector('.todo-list')
+const todoComplete = document.querySelector('.todo-completed')
+
+let todoData = JSON.parse(localStorage.getItem('todo'))
+
+const render = () => {
+
+	todoList.innerHTML = ''
+	todoComplete.innerHTML = ''
+	todoData.map((item, index) => {
+		const li = document.createElement('li')
+		li.classList.add('todo-item')
+		li.innerHTML =
+			`<span class="text-todo">${item.text}</span>
+				<div class="todo-buttons">
+					<button class="todo-remove"></button>
+					<button class="todo-complete"></button>
+				</div>
+			`
+		if (item.completed) {
+			todoComplete.append(li)
+		} else {
+			todoList.append(li)
+		}
+
+		li.querySelector('.todo-remove').addEventListener('click', () => {
+			const removeTodo = index
+			todoData = todoData.filter((_, index) => index !== removeTodo)
+			render()
+		})
+		li.querySelector('.todo-complete').addEventListener('click', () => {
+			item.completed = !item.completed
+			render()
+		})
+	})
+	localStorage.setItem('todo', JSON.stringify(todoData))
+}
+
+
+todoControl.addEventListener('submit', function (e) {
+	e.preventDefault()
+	if (headerInput.value.trim() !== '') {
+		const newTodo = {
+			text: headerInput.value,
+			completed: false
+		}
+		todoData.push(newTodo)
+		headerInput.value = ''
+		render()
+	}
+})
+
+render()
